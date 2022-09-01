ff5d96037a2228436540eaa9fbfecda4c26aa674
Udith <udithmihiranga@gmail.com>
2022-09-01 21:23:14 +0530

    Implemented hide input feilds functionality.

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [isTyping, setIsTyping] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsTyping(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsTyping(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsTyping(false);
   };
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isTyping && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isTyping && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
